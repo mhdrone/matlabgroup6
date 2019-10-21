faf1e5b5da164e5fb5b09599eb83d2255e791758
mhdrone <mhdrone557@gmail.com>
2019-10-20 23:55:43 -0400

    another one

--- M2_P2_decision.m
+++ M2_P2_decision.m
@@ -1,7 +1,21 @@
-%Module 2 Part 2 Group 6 Decision Program
+% Module 2 Part 2 Group 6 Decision Program
+% Mario ---- 
+% Stephen -- 
+% Brett ---- 
+% Vedant --- 
+% Edward --- 
+% Hamilton - Basic program layout, final formating
 
 clear                                                                       %clear all varables
 clc                                                                         %clear the command window
 
 load WaterUseData.mat                                                       %load water useage file
 
+ActiveMetaData = ThermoelectricpowerinBgald;                                %set active metadata
+
+% the following code is based of the first question "at what point should
+% investers stop investing in thermoelectirc power based off the given data
+
+% plan: to graph the data points and then generate a line of best fit, find
+% out where the derivative of the line becomes negative and report the 
+% value of x right before that point
