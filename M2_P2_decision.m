691ac52b210804ec7eecee6496d43fb6d575c771
mhdrone <mhdrone557@gmail.com>
2019-10-21 09:57:26 -0400

    added graphs

--- M2_P2_decision.m
+++ M2_P2_decision.m
@@ -1,5 +1,5 @@
 % Module 2 Part 2 Group 6 Decision Program
-% Mario ---- 
+% Mario ---- outputing data to file
 % Stephen -- 
 % Brett ---- 
 % Vedant --- 
@@ -8,10 +8,17 @@
 
 clear                                                                       %clear all varables
 clc                                                                         %clear the command window
+close all;
 
 load WaterUseData.mat                                                       %load water useage file
 
 ActiveMetaData = ThermoelectricpowerinBgald;                                %set active metadata
+
+y = 1;                                                                      %set y = 1
+n = 0;                                                                      %set n = 0
+
+GraphforUser = input('Would you like to display graphs? [y/n]: ');          %ask user if they would like to display graphs
+SaveFileUser = input('Would you like to output data to file? [y/n]: ');     %ask user if they would like to output to file
 
 % the following code is based of the first question "at what point should
 % investers stop investing in thermoelectirc power based off the given data
@@ -20,6 +27,19 @@ ActiveMetaData = ThermoelectricpowerinBgald;                                %set
 % out where the derivative of the line becomes negative and report the 
 % value of x right before that point
 
-BestFit = polyfit(Year, SelMetaData, LineType);                             %Generate line of best fit
+BestFit = polyfit(Year, ActiveMetaData, 2);                                 %Generate line of best fit
 BestFitLine = polyval(BestFit, Year);                                       %Clean up best fit line into a usable format
+BestFitDer = polyder(BestFit);                                              %Get the derivitive of the best fit line
+BestFitDerLine = polyval(BestFitDer, Year);
+%BestFitDerLine = (BestFitDer(1)*Year)+BestFitLine(2);                       %Put the derivitive into a usable format
 
+if GraphforUser == 1
+    hold on;
+    Plot1 = plot(Year, ActiveMetaData, 'ro');
+    Plot2 = plot(Year, BestFitLine, 'b');
+    Plot3 = plot(Year, BestFitDerLine);
+end
+
+if SaveFileUser == 1
+    disp("sorry, saving to file hasn't been added yet");
+end
